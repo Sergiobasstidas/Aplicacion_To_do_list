6585c8643d7aecd0721488b60f1b8658680ac6be
Sergio Bastidas <sergio.batidas@gmail.com>
2021-08-01 12:47:44 -0400

    Desafio Trabajando con props

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,15 @@
 <template>
-  <div id="app">
-    <Titulo/>
-    <label >Tarea</label>
-    <input type="text" placeholder="Agregar nueva tarea" v-model="nuevaTarea"/>
-    <button @click="agregarTarea()">Crear</button>
-
-    <h2>Lista</h2>
-    <ul>
-<li v-for="(tarea, $index) in tareas" :key="$index">
-        <div v-if="!tarea.modificable">{{ $index }}: {{ tarea.texto }}</div>
-        <input v-if="tarea.modificable" type="text" v-model="tarea.texto" />
-        <button @click="modificar(tarea)">M</button>
-      </li>
-    </ul>
+  <div>
+    <Todo :title="'Lista del supermercado'" />
   </div>
 </template>
 
 <script>
-import Titulo from '@/components/Titulo.vue'
+import Todo from '@/components/Todo.vue'
 export default {
   name: 'App',
   components: {
-    Titulo,
-  },
-  data: () => ({
-    nuevaTarea: '',
-    tareas: [],
-  }),
-  methods: {
-    agregarTarea() {
-      this.tareas.push({ texto: this.nuevaTarea, modificable: false })
-      this.nuevaTarea = ''
-    },
-    modificar(tarea) {
-      if (!tarea.modificable) {
-        tarea.modificable = true
-      } else {
-        tarea.modificable = false
-      }
-    },
+    Todo,
   },
 }
 </script>
-
-<style>
-
-</style>
